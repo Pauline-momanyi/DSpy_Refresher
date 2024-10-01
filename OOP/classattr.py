0a9706381613e9c4cebb9ed7b1dd680469511085
Pauline Momanyi <64144317+Pauline-momanyi@users.noreply.github.com>
2024-10-01 08:19:22 -0700

    Using class method in init method

--- OOP/classattr.py
+++ OOP/classattr.py
@@ -3,7 +3,8 @@ class Person:
     def __init__(self, name) -> None:
         self.name = name
         
-    # class methosd: Don't reference self but the class itself
+    # class method: Don't reference self but the class itself
+    # You can use a method to call it but you dont have to
     
     @classmethod
     def get_number(cls):
@@ -20,3 +21,30 @@ print (Person.number)
 
 print (Person.get_number())
 print (Person.add_person())
+print("End of first")
+
+class Person1:
+    number = 0 #specific to class i.e class attribute
+    def __init__(self, name) -> None:
+        self.name = name
+        # the class method below can be used here
+        Person1.add_person()
+        
+    # class method: Don't reference self but the class itself
+    # You can use a method to call it but you dont have to
+    
+    @classmethod
+    def get_number(cls):
+        return cls.number
+    
+    @classmethod
+    def add_person(cls):
+        cls.number += 1
+        return cls.number
+        
+p11 = Person1("popo")
+print (p11.number)
+print (Person1.number)  
+
+print (Person1.get_number())
+print (Person1.add_person())
